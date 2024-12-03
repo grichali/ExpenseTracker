6a85fbcb46c4cd51ea8e032f3fc098032d4c047a
Ali Grich <128514098+grichali@users.noreply.github.com>
2024-12-03 16:17:41 +0100

    add splashActivity

---
+++ app/src/main/java/com/example/expensetracker/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.expensetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // Navigate to Dashboard
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Close SplashActivity
+        }, 3000); // 3 sec delay
+    }
+
+}
